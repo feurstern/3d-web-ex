9c32a2d16158f60b5f8a0ef81c5a0752425dd89e
梨央Rio <rio-kurniawan@sakura-schutzstaffel.com>
2024-03-31 10:41:21 +0700

    create navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas } from './components'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from './components'
 
 
 const Home = () => {
   return (
     <div className='relative z-0 bg-primary'>
       <div className='bg-hero-patter bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />    
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+
       </div>
 
     </div>
