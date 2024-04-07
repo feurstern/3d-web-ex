6f8c69bb1b48fe9624cce2ec86f97283acc574fb
梨央Rio <rio-kurniawan@sakura-schutzstaffel.com>
2024-04-07 20:53:48 +0700

    installing the required dependencies, and try to debug the error of module not found

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -10,11 +10,19 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex-row items-start gap-5 `}>
         <div className='flex flex-col justify-center items-center mt-5'>
-            <div className='w-5 h-5 rounded-full bg-sky-600'/>
-            <div className='w-1 sm:h-80 h-40 bg-red-600' />
+          <div className='w-5 h-5 rounded-full bg-sky-600' />
+          <div className='w-1 sm:h-80 h-40 bg-red-600' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>Hi, I'm <span className='text-[#915eff]'>Hatsune Miku</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Testing how to develope 3d website <br className='sm:block hidden' />
+            Learn how to develop a game
+          </p>
         </div>
       </div>
-    
+
+      <ComputersCanvas />
 
     </section>
   )
--- src/app/components/canvas/Computers.jsx
+++ src/app/components/canvas/Computers.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+"use client"
+
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+// import { OrbitControlss, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader';
+import { useGLTF } from 'drei';
 
 const Computers = () => {
+  const computer3d = useGLTF('./desktop_pc/scene.gltf');
+
   return (
     <div>Computers</div>
   )
